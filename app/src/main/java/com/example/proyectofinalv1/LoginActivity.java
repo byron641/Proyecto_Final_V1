7c3d7973465b0071d401b0a03d2b139ba7650489
Bayron Dominguez <166480248+byron641@users.noreply.github.com>
2024-07-31 12:10:20 -0600

    Google Sign partially work now

--- app/src/main/java/com/example/proyectofinalv1/LoginActivity.java
+++ app/src/main/java/com/example/proyectofinalv1/LoginActivity.java
@@ -4,16 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -22,20 +18,17 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.Firebase;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-
 public class LoginActivity extends AppCompatActivity {
 
-    private static final int RC_SIGN_IN=9001;
+    private static final int RC_SIGN_IN = 9001;
     private GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth mAuth;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,32 +48,34 @@ public class LoginActivity extends AppCompatActivity {
         ImageButton SignInButton = findViewById(R.id.BtnGoogleSign);
         SignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {SignIn();
+            public void onClick(View v) {
+                SignIn();
             }
         });
 
     }
+
     private void SignIn() {
         Intent SignInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(SignInIntent, RC_SIGN_IN);
     }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        LoginActivity.super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == RC_SIGN_IN){
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 firebaseAuthWithGoogle(account.getIdToken());
-            } catch (ApiException e){
-                Log.w("LoginActivity", e.getMessage());
+            } catch (ApiException e) {
+                Log.w("LoginActivity", e.getMessage(), e);
             }
         }
-
     }
 
-    private void firebaseAuthWithGoogle(String idToken){
+    private void firebaseAuthWithGoogle(String idToken) {
         AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -92,13 +87,10 @@ public class LoginActivity extends AppCompatActivity {
                             Intent main = new Intent(LoginActivity.this, MainActivity.class);
                             startActivity(main);
                             finish();
-                        }else{
-                            Toast.makeText(LoginActivity.this, "Error en el login",Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Error en el login", Toast.LENGTH_SHORT).show();
                         }
-                        }
-
+                    }
                 });
     }
-
-
-    }
+}
